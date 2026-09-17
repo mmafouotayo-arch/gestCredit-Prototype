036c1b90cdf6dff290f9d67b883dec73134de641
mmafouotayo-arch <mmafouotayo@gmail.com>
2026-09-17 12:12:51 +0100

    feat: ajout du menu principal avec gestion de saisie invalide

---
+++ GestCredit.Prototype/Program.cs
@@ -0,0 +1,33 @@
+﻿bool continuer = true;
+
+while (continuer)
+{
+    Console.WriteLine("GestCredit");
+    Console.WriteLine("1. Option 1");
+    Console.WriteLine("2. Option 2");
+    Console.WriteLine("3. Option 3");
+    Console.WriteLine("4. Quitter");
+    Console.Write("Votre choix : ");
+
+    string? saisie = Console.ReadLine();
+
+    switch (saisie)
+    {
+        case "1":
+            Console.WriteLine("Vous avez choisi l'option 1");
+            break;
+        case "2":
+            Console.WriteLine("Vous avez choisi l'option 2");
+            break;
+        case "3":
+            Console.WriteLine("Vous avez choisi l'option 3");
+            break;
+        case "4":
+            continuer = false;
+            Console.WriteLine("Au revoir !");
+            break;
+        default:
+            Console.WriteLine("Saisie invalide, veuillez réessayer.");
+            break;
+    }
+}
---
+++ GestCredit.Prototype/README.md
@@ -0,0 +1,13 @@
+# GestCredit - Prototype
+
+Prototype console développé pendant la Semaine 1 de la montée en compétence Full-Stack .NET / Next.js.
+
+## Contenu
+- Menu principal avec gestion de saisie invalide
+- Calculateur de mensualité de prêt
+- Gestion d'une liste de clients en mémoire via LINQ
+
+## Lancer le projet
+```bash
+dotnet run
+```
